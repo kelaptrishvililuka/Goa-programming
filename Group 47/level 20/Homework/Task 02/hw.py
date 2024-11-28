1b58216ab3c35938a1aaa204814fec69365cc060
kelaptrishvililuka <kelaptrishvililuka91@gmail.com>
2024-11-28 21:26:44 +0400

    Delete Group 47/level 20 directory

--- Group 47/level 20/Homework/Task 02/hw.py
+++
@@ -1,5 +0,0 @@
-# შექმნი სია სადაც იქნება სადაც იანქბა ციფრები რომელიც გამოიტყანს 5 სა და 3ის ჯერადევბს
-
-numbers = list(range(1, 101))
-multiples_of_5_and_3 = [num for num in numbers if num % 5 == 0 and num % 3 == 0]
-print("Multiples of 5 and 3: {multiples_of_5_and_3}")

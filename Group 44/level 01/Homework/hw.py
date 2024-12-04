9d28eb887a70d55600e85765e7d7620bc9a54ecf
kelaptrishvililuka <kelaptrishvililuka91@gmail.com>
2024-12-04 17:31:01 +0400

    commit

--- Group 44/level 01/Homework/hw.py
+++ Group 44/level 01/Homework/hw.py
@@ -1,1 +1,4 @@
-# nothing
+name = ("luka")
+surname = (' keleptrishvili ')
+height = ('175')
+print("hi my name is " + name + surname + " my height is " + height +"cm")
---
+++ Group 44/level 06/Classwork/Task 03/cw.py
@@ -0,0 +1,5 @@
+# 3)მომხმარებელს შემოატანინეთ მისი სახელი, შემდეგ შემოატანინეთ რაღაც რიცხვი და გამოუტანეთ მისი სახელი გამრავლებული ამ რიცხვზე
+
+name = input("enter your name: ")
+number = int(input("enter your number: "))
+print(name * number)

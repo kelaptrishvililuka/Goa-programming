985605f0ef1b88c6009c76151cf325787f4186c5
kelaptrishvililuka <kelaptrishvililuka91@gmail.com>
2025-01-07 01:31:12 +0400

    commit

--- Group 57/level 28/Homework/hw3.py
+++ Group 57/level 28/Homework/hw3.py
@@ -1,6 +1,6 @@
 # 3) შექმენით სია, რომელიც შეიცავს 6 რიცხვს. გამოიტანეთ სიის ელემენტების ჯამი და საშუალო მნიშვნელობა.
 
-numbers = [10, 20, 30, 40, 50, 60]
+numbers = [1, 2, 3, 4, 5, 6]
 sum_of_numbers = sum(numbers)
 average = sum_of_numbers / len(numbers)
 
--- Group 57/level 28/Homework/hw4.py
+++ Group 57/level 28/Homework/hw4.py
@@ -1,8 +1,8 @@
 # 4) შექმენით სია, რომელიც შეიცავს 8 სხვადასხვა ქალაქის სახელებს. წაშალეთ მესამე და მეხუთე ქალაქის სახელი და გამოიტანეთ განახლებული სია.
 
-cities = ["gori", "foti", "qobuleti", "Mtskheta", "Kutaisi", "dusheti", "borjomi"]
+cities = ["gori", "foti", "qobuleti", "Mtskheta", "Kutaisi", "dusheti", "borjomi", "xashuri"]
 
-cities.pop(2)
 cities.pop(3)
+cities.pop(-4)
 
 print(cities)
